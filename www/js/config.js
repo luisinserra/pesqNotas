bc85a2cdb1fe56e09844f3e11ad0ecd587416844
Luis Inserra <luisinserra@gmail.com>
2018-03-28 18:09:55 -0300

    atualização 1

--- www/js/config.js
+++ www/js/config.js
@@ -12,18 +12,23 @@ function getPastaAtual(){
 function goBuscaPasta() {
 	var pastaAtual=getPastaAtual();
 	var pasta='file://'+pastaAtual;
+	alert("Pasta: "+pasta);
 	window.resolveLocalFileSystemURI(pasta, pastaSucesso, pastaErro);
 }
 function pastaSucesso(entry){
+	alert("Iniciando o scan...");
 	if (entry.isFile) {
+		alert("Achei um arquivo...");
             //fileHandler( entry );
             var nome=entry.name;
             var msg="arquivo "+nome;
         } else {
+        	alert("Achei uma pasta...");
         	var nome=entry.name;
         	var msg="pasta "+nome;
         }
         var resultado=document.getElementById('spanResposta').innerHTML;
+        alert("já pegou um resultado...");
         if (resultado != ''){
         	resultado+='<br>';
         }
