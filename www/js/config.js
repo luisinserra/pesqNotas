1786bb89f4c23973fa1bb7a20b786392acd4c7a4
Luis Inserra <luisinserra@gmail.com>
2018-03-28 18:34:44 -0300

    atualização 3

--- www/js/config.js
+++ www/js/config.js
@@ -10,11 +10,47 @@ function getPastaAtual(){
 	return pastaAtual;
 }
 function goBuscaPasta() {
+/*	
 	var pastaAtual=document.getElementById('tPasta').value;
 	var pasta='file://'+pastaAtual;
 	alert("Pasta: "+pasta);
 	window.resolveLocalFileSystemURI(pasta, pastaSucesso, pastaErro);
+*/
+	window.requestFileSystem(LocalFileSystem.PERSISTENT,0,  onFileSystemSuccess, onErrorRead);
 }
+function onFileSystemSuccess(fs) {
+    alert("Sucesso");
+    var pathInicial=fs.root.fullPath;
+    alert("Entrando com "+pathInicial+"...");
+    fs.root.fullPath = '/';
+    alert("mudou o path...");
+    var dirReader = fs.root.createReader();
+    alert("reader criado para ler de "+fs.root.fullPath+"...");
+    dirReader.readEntries(successRead,onErrorRead);
+}
+function successRead(entries){
+    alert("sucesso lendo");
+     var i;
+     var objectType;
+     var n=entries.length;
+     alert("varrendo "+n+"...");
+     for (i=0; i < entries.length; i++) {
+        if(entries[i].isDirectory == true) {
+            objectType = 'Directory';
+        } else {
+            objectType = 'File';
+            alert("arquivo");
+            alert(entries[i].name);
+        }
+        $('#dirList').append('<li><h3>' + entries[i].name + '</h3><p>' + entries[i].toURI() + '</p><p class="ui-li-aside">Type:<strong>' + objectType + '</strong></p></li>');
+    }
+    $('#dirList').listview("refresh");
+}
+
+function onErrorRead(error) {
+    alert("Failed to list directory contents: " + error.code+","+error.message);
+}
+
 function pastaSucesso(entry){
 	alert("Iniciando o scan...");
 	if (entry.isFile) {
